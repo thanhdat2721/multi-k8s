5e6587f8779953a1f53a4104a24f881879eb83d9
thanhdat2721 <nguyenthanhdat2721@gmail.com>
2020-09-24 23:39:22 -0500

    change deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,15 @@
 docker build -t thanhdat27/multi-client:latest -t thanhdat27/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t thanhdat27/multi-server:latest - t thanhdat27/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t thanhdat27/multi-server:latest -t thanhdat27/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t thanhdat27/multi-worker:latest -t thanhdat27/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
 docker push thanhdat27/multi-client:latest
 docker push thanhdat27/multi-server:latest
 docker push thanhdat27/multi-worker:latest
+
 docker push thanhdat27/multi-client:$SHA
 docker push thanhdat27/multi-server:$SHA
 docker push thanhdat27/multi-worker:$SHA
+
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=thanhdat27/multi-server:$SHA
 kubectl set image deployments/client-deployment client=thanhdat27/multi-client:$SHA
